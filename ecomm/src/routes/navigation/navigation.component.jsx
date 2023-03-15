8599728696c329114dce764646cb277a8dec8085
jclem28 <jclem2806@gmail.com>
2023-03-15 14:53:23 -0500

    added sign in functionality, combined sign in form and sign up form into one component

--- ecomm/src/routes/navigation/navigation.component.jsx
+++ ecomm/src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,6 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
-import { ReactComponent as CrwnLogo } from "../../../assets/crown.svg";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
 import "./navigation.style.scss";
 
@@ -15,7 +15,7 @@ const Navigation = () => {
           <Link className="nav-link" to="/shop">
             SHOP
           </Link>
-          <Link className="nav-link" to="/sign-in">
+          <Link className="nav-link" to="/auth">
             SIGN IN
           </Link>
         </div>
